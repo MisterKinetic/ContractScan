c16b052ff7346c374b49eef599629cc2a94ba0fe
MisterKinetic <ahmedzamir1111@gmail.com>
2026-04-05 07:27:36 +0530

    Fix Linux path compatibility and env variable loading

--- backend/src/main/java/com/contractscan/backend/BackendApplication.java
+++ backend/src/main/java/com/contractscan/backend/BackendApplication.java
@@ -4,10 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
-
 import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
@@ -28,14 +24,5 @@ public class BackendApplication {
             System.err.println("Could not load .env: " + e.getMessage());
         }
         SpringApplication.run(BackendApplication.class, args);
-    }
-
-    @Bean
-    public CommandLineRunner debugEnv(Environment env) {
-        return args -> {
-            System.out.println("DEBUG: GOOGLE_CLIENT_ID = " + env.getProperty("GOOGLE_CLIENT_ID"));
-            System.out.println("DEBUG: oauth2 client-id = "
-                    + env.getProperty("spring.security.oauth2.client.registration.google.client-id"));
-        };
     }
 }
